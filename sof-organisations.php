c318b71691f35c357bab99c81244182e93ba6ff2
Christian Wach <needle@haystack.co.uk>
2025-08-22 20:33:51 +0100

    Bump version to 1.0.1

--- sof-organisations.php
+++ sof-organisations.php
@@ -4,7 +4,7 @@
  *
  * Plugin Name:       SOF Organisations
  * Description:       Provides Organisation Custom Post Types for The Ball website.
- * Version:           1.0.1a
+ * Version:           1.0.1
  * Plugin URI:        https://github.com/spiritoffootball/sof-organisations
  * GitHub Plugin URI: https://github.com/spiritoffootball/sof-organisations
  * Author:            Christian Wach
@@ -19,7 +19,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Set our version here.
-define( 'SOF_ORGANISATIONS_VERSION', '1.0.1a' );
+define( 'SOF_ORGANISATIONS_VERSION', '1.0.1' );
 
 // Store reference to this file.
 if ( ! defined( 'SOF_ORGANISATIONS_FILE' ) ) {
